b475ef72c8eb837f024fcd4b9cb2e39a6ba0596e
Ahmed Oday <88116409+AhmedAuday@users.noreply.github.com>
2024-05-11 03:18:13 +0300

    Added AnimatablePluginProvider

--- client/xibot/src/contexts/PluginContext.tsx
+++ client/xibot/src/contexts/PluginContext.tsx
@@ -1,28 +1,33 @@
-import React, {useContext , useState } from 'react'
+/* eslint-disable react-refresh/only-export-components */
+import React, { ReactNode, useContext, useState } from "react";
 
 const AnimeQuotePluginContext = React.createContext(true);
-const AnimeQuotePluginContextUpdate = React.createContext(()=>{});
+const AnimeQuotePluginContextUpdate = React.createContext(() => {});
 
 export function useAnimeQuotePlugin() {
-    return useContext(AnimeQuotePluginContext)
+  return useContext(AnimeQuotePluginContext);
 }
 
 export function useAnimeQuotePluginUpdate() {
-    return useContext(AnimeQuotePluginContextUpdate)
+  return useContext(AnimeQuotePluginContextUpdate);
 }
+interface AnimeQuotePluginProviderProps {
+  children: ReactNode;
+}
+export function AnimeQuotePluginProvider({
+  children,
+}: AnimeQuotePluginProviderProps) {
+  const [isAnimeQuoteActive, toggleAnimeQuote] = useState(true);
 
-export function AnimeQuotePluginProvider({ children }) {
-    const [isAnimeQuoteActive , toggleAnimeQuote]=useState(true)
+  function toggleAnimeQuotePlugin() {
+    toggleAnimeQuote((prevIsAnimeQuoteActive) => !prevIsAnimeQuoteActive);
+  }
 
-    function toggleAnimeQuotePlugin(){
-        toggleAnimeQuote(prevIsAnimeQuoteActive=> !prevIsAnimeQuoteActive)
-    }
-    
-    return( 
-            <AnimeQuotePluginContext.Provider value={isAnimeQuoteActive}>
-                <AnimeQuotePluginContextUpdate.Provider value={toggleAnimeQuotePlugin}>
-                    {children}
-                </AnimeQuotePluginContextUpdate.Provider>
-            </AnimeQuotePluginContext.Provider>
-        )
-}           
+  return (
+    <AnimeQuotePluginContext.Provider value={isAnimeQuoteActive}>
+      <AnimeQuotePluginContextUpdate.Provider value={toggleAnimeQuotePlugin}>
+        {children}
+      </AnimeQuotePluginContextUpdate.Provider>
+    </AnimeQuotePluginContext.Provider>
+  );
+}
